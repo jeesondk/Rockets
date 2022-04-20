9fb6e0c93f840ee68a7678ae3515f3eeb3657354
Jesper Jensen <jesperjensen@deltek.com>
2022-04-20 13:37:21 +0200

    test: Added error handling add additional test cases

--- services/messageHandler.go
+++ services/messageHandler.go
@@ -2,28 +2,42 @@ package services
 
 import (
 	DTO "RocketService/dto"
+	"fmt"
 )
 
 type MessageHandler struct {
 }
 
-func (m *MessageHandler) HandleLaunchMessage(data interface{}) DTO.RocketLaunched {
+func (m *MessageHandler) HandleLaunchMessage(data interface{}) (DTO.RocketLaunched, error) {
 	d := data.(map[string]interface{})
 
 	msg := DTO.RocketLaunched{}
+	errMsg := ""
 
 	if rocketType, ok := d["type"].(string); ok {
 		msg.Type = rocketType
+	} else {
+		errMsg += "Unable to parse type. "
 	}
+
 	if mission, ok := d["mission"].(string); ok {
 		msg.Mission = mission
+	} else {
+		errMsg += "Unable to parse mission. "
 	}
 
 	if launchSpeed, ok := d["launchSpeed"].(int); ok {
 		msg.LaunchSpeed = launchSpeed
+	} else {
+		errMsg += "Unable to parse launchSpeed. "
 	}
 
-	return msg
+	if errMsg != "" {
+		err := fmt.Errorf("caught errors while parsing data: %s", errMsg)
+		return msg, err
+	}
+
+	return msg, nil
 }
 
 func NewMessageHandler() *MessageHandler {
--- tests/messageHandler_test.go
+++ tests/messageHandler_test.go
@@ -20,8 +20,66 @@ func TestCanHandleLaunchMsg(t *testing.T) {
 	}
 
 	h := services.NewMessageHandler()
-	resp := h.HandleLaunchMessage(message)
+	resp, err := h.HandleLaunchMessage(message)
 
+	assert.Nil(t, err)
+	assert.Equal(t, expected, resp)
+
+}
+
+func TestFailHandleLaunchMsgTypeErr(t *testing.T) {
+	message := make(map[string]interface{})
+	message["type"] = 99
+	message["launchSpeed"] = 500
+	message["mission"] = "ARTEMIS"
+
+	expected := DTO.RocketLaunched{
+		LaunchSpeed: 500,
+		Mission:     "ARTEMIS",
+	}
+
+	h := services.NewMessageHandler()
+	resp, err := h.HandleLaunchMessage(message)
+
+	assert.Error(t, err, "caught errors while parsing data: Unable to parse type. ")
+	assert.Equal(t, expected, resp)
+
+}
+
+func TestFailHandleLaunchMsgMissionErr(t *testing.T) {
+	message := make(map[string]interface{})
+	message["type"] = "Falcon-9"
+	message["launchSpeed"] = 500
+	message["mission"] = 42
+
+	expected := DTO.RocketLaunched{
+		Type:        "Falcon-9",
+		LaunchSpeed: 500,
+	}
+
+	h := services.NewMessageHandler()
+	resp, err := h.HandleLaunchMessage(message)
+
+	assert.Error(t, err, "caught errors while parsing data: Unable to parse mission. ")
+	assert.Equal(t, expected, resp)
+
+}
+
+func TestFailHandleLaunchMsgSpeedErr(t *testing.T) {
+	message := make(map[string]interface{})
+	message["type"] = "Falcon-9"
+	message["launchSpeed"] = "500"
+	message["mission"] = "ARTEMIS"
+
+	expected := DTO.RocketLaunched{
+		Type:    "Falcon-9",
+		Mission: "ARTEMIS",
+	}
+
+	h := services.NewMessageHandler()
+	resp, err := h.HandleLaunchMessage(message)
+
+	assert.Error(t, err, "caught errors while parsing data: Unable to parse launchSpeed. ")
 	assert.Equal(t, expected, resp)
 
 }
