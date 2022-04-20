3e355d9cc671df73c9c4e248c0001fa08d1c07b2
Jesper Jensen <jesperjensen@deltek.com>
2022-04-20 21:38:26 +0200

    chore: renamed files to follow project std.

--- tests/messageService_test.go
+++ tests/messageService_test.go
@@ -2,9 +2,12 @@ package tests
 
 import (
 	DTO "RocketService/dto"
+	"RocketService/entities"
 	"RocketService/services"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestCanHandleLaunchMsg(t *testing.T) {
@@ -19,7 +22,7 @@ func TestCanHandleLaunchMsg(t *testing.T) {
 		Mission:     "ARTEMIS",
 	}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleLaunchMessage(message)
 
 	assert.Nil(t, err)
@@ -39,7 +42,7 @@ func TestCanHandleLaunchNegSpeedMsg(t *testing.T) {
 		Mission:     "ARTEMIS",
 	}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleLaunchMessage(message)
 
 	assert.Nil(t, err)
@@ -58,7 +61,7 @@ func TestFailHandleLaunchMsgTypeErr(t *testing.T) {
 		Mission:     "ARTEMIS",
 	}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleLaunchMessage(message)
 
 	assert.Error(t, err, "caught errors while parsing data: Unable to parse type. ")
@@ -77,7 +80,7 @@ func TestFailHandleLaunchMsgMissionErr(t *testing.T) {
 		LaunchSpeed: 500,
 	}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleLaunchMessage(message)
 
 	assert.Error(t, err, "caught errors while parsing data: Unable to parse mission. ")
@@ -96,7 +99,7 @@ func TestFailHandleLaunchMsgSpeedErr(t *testing.T) {
 		Mission: "ARTEMIS",
 	}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleLaunchMessage(message)
 
 	assert.Error(t, err, "caught errors while parsing data: Unable to parse launchSpeed. ")
@@ -112,7 +115,7 @@ func TestCanHandleSpeedIncreaseMsg(t *testing.T) {
 		By: 2500,
 	}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleSpeedIncreaseMessage(message)
 
 	assert.Nil(t, err)
@@ -128,7 +131,7 @@ func TestCanHandleSpeedDecreaseMsg(t *testing.T) {
 		By: 1500,
 	}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleSpeedDecreaseMessage(message)
 
 	assert.Nil(t, err)
@@ -144,7 +147,7 @@ func TestCanHandleNegSpeedIncreaseMsg(t *testing.T) {
 		By: 2500,
 	}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleSpeedIncreaseMessage(message)
 
 	assert.Nil(t, err)
@@ -160,7 +163,7 @@ func TestCanHandleNegSpeedDecreaseMsg(t *testing.T) {
 		By: 1500,
 	}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleSpeedDecreaseMessage(message)
 
 	assert.Nil(t, err)
@@ -174,7 +177,7 @@ func TestFailHandleSpeedIncreaseMsg(t *testing.T) {
 
 	expected := DTO.RocketSpeedIncreased{}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleSpeedIncreaseMessage(message)
 
 	assert.Error(t, err, "caught errors while parsing data: Unable to parse speed. ")
@@ -188,7 +191,7 @@ func TestFailHandleSpeedDecreaseMsg(t *testing.T) {
 
 	expected := DTO.RocketSpeedDecreased{}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleSpeedDecreaseMessage(message)
 
 	assert.Error(t, err, "caught errors while parsing data: Unable to parse speed. ")
@@ -204,7 +207,7 @@ func TestCanHandleNewMissionMsg(t *testing.T) {
 		NewMission: "SHUTTLE_MIR",
 	}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleMissionChangedMessage(message)
 
 	assert.Nil(t, err)
@@ -218,7 +221,7 @@ func TestFailHandleNewMissionMsg(t *testing.T) {
 
 	expected := DTO.RocketMissionChanged{}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleMissionChangedMessage(message)
 
 	assert.Error(t, err, "caught errors while parsing data: Unable to parse mission. ")
@@ -234,7 +237,7 @@ func TestCanHandleExplodedMsg(t *testing.T) {
 		Reason: "PRESSURE_VESSEL_FAILURE",
 	}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleRocketExplodedMessage(message)
 
 	assert.Nil(t, err)
@@ -248,10 +251,112 @@ func TestFailHandleExplodedMsg(t *testing.T) {
 
 	expected := DTO.RocketExploded{}
 
-	h := services.NewMessageHandler()
+	h := services.NewMessageService()
 	resp, err := h.HandleRocketExplodedMessage(message)
 
 	assert.Error(t, err, "caught errors while parsing data: Unable to parse reason. ")
 	assert.Equal(t, expected, resp)
 
+}
+
+func TestMessageHandleMessages(t *testing.T) {
+	rocket := entities.Rocket{
+		ID:   "1",
+		Type: "Falcon 9",
+		Speed: entities.RocketSpeed{
+			Current: 0,
+			Max:     10000,
+		},
+		Mission:    "ARTEMIS",
+		LaunchDate: time.Now(),
+	}
+
+	tests := []struct {
+		name     string
+		rocket   entities.Rocket
+		metadata DTO.MetaData
+		message  map[string]interface{}
+		expected entities.Rocket
+	}{
+		{"CAN_HANDLE_SPEED_INCREASE_MSG",
+			rocket,
+			DTO.MetaData{
+				Channel:       "rocket_id",
+				MessageType:   "SPEED_INCREASE",
+				MessageNumber: 1,
+				MessageTime:   time.Now().String(),
+			},
+			map[string]interface{}{
+				"by": 2500,
+			},
+			entities.Rocket{
+				ID: "rocket_id",
+				Speed: entities.RocketSpeed{
+					Current: 3500,
+					Max:     10000,
+				},
+			},
+		},
+		{"CAN_HANDLE_SPEED_DECREASE_MSG",
+			rocket,
+			DTO.MetaData{
+				Channel:       "rocket_id",
+				MessageType:   "SPEED_DECREASE",
+				MessageNumber: 1,
+				MessageTime:   time.Now().String(),
+			},
+			map[string]interface{}{
+				"by": -1500,
+			},
+			entities.Rocket{
+				ID: "rocket_id",
+				Speed: entities.RocketSpeed{
+					Current: 3500,
+					Max:     10000,
+				},
+			},
+		},
+		{"CAN_HANDLE_MISSION_CHANGED_MSG",
+			rocket,
+			DTO.MetaData{
+				Channel:       "rocket_id",
+				MessageType:   "MISSION_CHANGED",
+				MessageNumber: 1,
+				MessageTime:   time.Now().String(),
+			},
+			map[string]interface{}{
+				"newMission": "SHUTTLE_MIR",
+			},
+			entities.Rocket{
+				ID:      "rocket_id",
+				Mission: "SHUTTLE_MIR",
+			},
+		},
+		{"CAN_HANDLE_EXPLODED_MSG",
+			rocket,
+			DTO.MetaData{
+				Channel:       "rocket_id",
+				MessageType:   "EXPLODED",
+				MessageNumber: 1,
+				MessageTime:   time.Now().String(),
+			},
+			map[string]interface{}{
+				"reason": "PRESSURE_VESSEL_FAILURE",
+			},
+			entities.Rocket{
+				ID:     "rocket_id",
+				Status: entities.RocketStatus{Active: false, Reason: "EXPLODED"},
+			},
+		},
+	}
+
+	for i, tc := range tests {
+		t.Run(fmt.Sprintf("Test %d: %s", i, tc.name), func(t *testing.T) {
+			s := services.NewMessageService()
+			resp, err := s.HandleMessage(tc.metadata, tc.message)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, resp)
+		})
+	}
 }
