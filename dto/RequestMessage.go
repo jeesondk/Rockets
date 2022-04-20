0bc8201c9ebffec269dcb0475c969ebc24df3d6c
Jesper Jensen <jesperjensen@deltek.com>
2022-04-20 11:54:02 +0200

    chore: extracted metadata struct, minor clean up, tests green

--- dto/RequestMessage.go
+++ dto/RequestMessage.go
@@ -3,11 +3,13 @@ package DTO
 import "RocketService/enum"
 
 type RequestMessage struct {
-	Metadata struct {
-		Channel       string           `json:"channel"`
-		MessageNumber int64            `json:"messageNumber"`
-		MessageTime   string           `json:"messageTime"`
-		MessageType   enum.MessageType `json:"messageType"`
-	} `json:"metadata"`
-	Message interface{} `json:"message"`
+	Metadata MetaData    `json:"metadata"`
+	Message  interface{} `json:"message"`
+}
+
+type MetaData struct {
+	Channel       string           `json:"channel"`
+	MessageNumber int64            `json:"messageNumber"`
+	MessageTime   string           `json:"messageTime"`
+	MessageType   enum.MessageType `json:"messageType"`
 }
